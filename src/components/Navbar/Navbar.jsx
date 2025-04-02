9f999ed695336f54e2f2024be0452cfa7997c086
Anshul <darkhunter1432004@gmail.com>
2025-04-02 22:32:23 +0530

    navbar updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,3 @@
-import PersonIcon from '@mui/icons-material/Person';
-
-
 import "./Navbar.css";
 export const Navbar = () => {
     return (
@@ -13,7 +10,9 @@ export const Navbar = () => {
         <nav className="d-flex align-center gap-large">
           <div className="nav d-flex align-center cursor-pointer">
             <span className="material-icons-outlined profile-option menu">menu</span>
-            <span className="profile-option menu"><PersonIcon/></span>
+            <span className="material-icons-outlined profile-option person">
+            person_2
+          </span>
           </div>
         </nav>
       </header>
