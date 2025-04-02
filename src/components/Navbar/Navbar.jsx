c5929c767932afb845164dd8f5171b0dc5bdab1b
Anshul <darkhunter1432004@gmail.com>
2025-04-02 17:24:34 +0530

    navbar structuring

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import PersonIcon from '@mui/icons-material/Person';
+
+
+import "./Navbar.css";
+export const Navbar = () => {
+    return (
+      <header className="heading d-flex align-center">
+        <h1 className="heading-1">
+          <a className="link" href="/">
+          VoyageVista
+          </a>
+        </h1>
+        <nav className="d-flex align-center gap-large">
+          <div className="nav d-flex align-center cursor-pointer">
+            <span className="material-icons-outlined profile-option menu">menu</span>
+            <span className="profile-option menu"><PersonIcon/></span>
+          </div>
+        </nav>
+      </header>
+    );
+  };
+  
