bcf837a9002c61b7cb6b5159fe6fd18bbd074212
Anshul <darkhunter1432004@gmail.com>
2025-12-13 01:54:10 +0530

    backend hosted link update

--- src/pages/SearchResults/SearchResults.js
+++ src/pages/SearchResults/SearchResults.js
@@ -13,7 +13,7 @@ export const SearchResults = () => {
     (async () => {
       try {
         const { data } = await axios.get(
-          `http://localhost:3200/api/hotels?categories=${hotelCategory}`
+          `https://travel-app-backend-zvzh.onrender.com/api/hotels?categories=${hotelCategory}`
         );
         setHotels(data);
       } catch (err) {
