cb4c047aa6aa9419bfc6771aa6f2f2d80a42743f
Anshul <darkhunter1432004@gmail.com>
2025-12-22 23:30:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import {
@@ -12,8 +12,7 @@ import {
   AlertProvider
 } from "./context";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <CategoryProvider>
@@ -32,5 +31,6 @@ root.render(
         </DateProvider>
       </CategoryProvider>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
